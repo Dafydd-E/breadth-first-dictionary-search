304ecc69d58cd256493f5ae56ef1253eac1b5154
Dafydd Evans <dafyddevans7@gmail.com>
2019-01-18 22:40:29 +0000

    Added Dictionary batching operations

---
+++ BluePrismChallenge/Application/DictionaryBatcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Application
+{
+    public class DictionaryBatcher
+    {
+        private StreamReader StreamReader { get; }
+
+        public DictionaryBatcher(StreamReader reader)
+        {
+            this.StreamReader = reader;
+        }
+
+        public string GetNextBatch()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            try
+            {
+                // TODO
+                for (int i = 0; i < 500; i++)
+                {
+                    builder.Append(this.StreamReader?.ReadLine() ?? throw new EndOfStreamException(""));
+                    builder.Append(";");
+                }
+            }
+            catch (EndOfStreamException)
+            {
+                // TODO
+                Console.WriteLine();
+            }
+            return builder.ToString();
+        }
+    }
+}
